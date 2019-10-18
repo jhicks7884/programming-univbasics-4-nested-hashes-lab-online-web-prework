0173f632a05aa46fb4b1311628a9fba8da8e9b75
Jeshua Hicks <jhicks7884@outlook.com>
2019-10-18 17:52:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,6 +59,9 @@ def dennis_ritchies_language
 end
 
 def adding_matz
+    programmer_hash.merge(:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP, C"]
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
